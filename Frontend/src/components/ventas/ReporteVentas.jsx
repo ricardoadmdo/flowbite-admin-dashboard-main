61437063ef17b555835ab179057e8bd84853b98f
Daniel González <daniogc37@gmail.com>
2024-12-22 00:34:16 -0500

    Agregar estilos CSS para mejorar la presentación de la tabla de Reporte de Ventas y asegurar su adaptabilidad en dispositivos móviles.

--- Frontend/src/components/ventas/ReporteVentas.jsx
+++ Frontend/src/components/ventas/ReporteVentas.jsx
@@ -7,207 +7,208 @@ import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Pagination from '../ui/Pagination';
 import { AuthContext } from '../../auth/authContext';
+import '../ventas/ReporteVentas.css'
 
 const ReporteVentas = () => {
-const {user} = useContext(AuthContext); 
-	
-	const [ventas, setVentas] = useState([]);
-	const [startDate, setStartDate] = useState(new Date());
-	const [totalGanancia, setTotalGanancia] = useState(0);
-	const [totalRecaudado, setTotalRecaudado] = useState(0);
-	const [productoMasVendido, setProductoMasVendido] = useState('');
-	const [currentPage, setCurrentPage] = useState(1);
-	const [totalPages, setTotalPages] = useState(1);
+  const { user } = useContext(AuthContext);
 
-	console.log(user)
-	// Fetch ventas con paginación y fecha seleccionada
-	useEffect(() => {
-		const fetchVentasByDateAndPage = async () => {
-			try {
-				const day = startDate.getDate();
-				const month = startDate.getMonth() + 1;
-				const year = startDate.getFullYear();
+  const [ventas, setVentas] = useState([]);
+  const [startDate, setStartDate] = useState(new Date());
+  const [totalGanancia, setTotalGanancia] = useState(0);
+  const [totalRecaudado, setTotalRecaudado] = useState(0);
+  const [productoMasVendido, setProductoMasVendido] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-				// Petición al backend con la fecha seleccionada y la página actual
-				const { data } = await Axios.get(`/venta?day=${day}&month=${month}&year=${year}&page=${currentPage}`);
+  // Fetch ventas con paginación y fecha seleccionada
+  useEffect(() => {
+    const fetchVentasByDateAndPage = async () => {
+      try {
+        const day = startDate.getDate();
+        const month = startDate.getMonth() + 1;
+        const year = startDate.getFullYear();
 
-				// Actualizar las ventas, total de páginas, etc.
-				setVentas(data.ventas);
-				setTotalPages(data.totalPages);
-				calcularGananciasYProductoMasVendido(data.ventas);
-			} catch (error) {
-				console.error('Error al obtener ventas:', error);
-			}
-		};
+        // Petición al backend con la fecha seleccionada y la página actual
+        const { data } = await Axios.get(`/venta?day=${day}&month=${month}&year=${year}&page=${currentPage}`);
 
-		fetchVentasByDateAndPage();
-	}, [startDate, currentPage]); // Llamar cada vez que cambie la fecha o la página actual
+        // Actualizar las ventas, total de páginas, etc.
+        setVentas(data.ventas);
+        setTotalPages(data.totalPages);
+        calcularGananciasYProductoMasVendido(data.ventas);
+      } catch (error) {
+        console.error('Error al obtener ventas:', error);
+      }
+    };
 
-	// Cálculo de ganancias, recaudado y producto más vendido
-	const calcularGananciasYProductoMasVendido = (ventas) => {
-		let totalGananciaCalculada = 0;
-		let totalRecaudadoCalculado = 0;
-		const productoContador = {};
+    fetchVentasByDateAndPage();
+  }, [startDate, currentPage]); // Llamar cada vez que cambie la fecha o la página actual
 
-		ventas.forEach((venta) => {
-			let gananciaVenta = 0;
+  // Modificar calcularGananciasYProductoMasVendido
+  const calcularGananciasYProductoMasVendido = (ventas) => {
+    let totalGananciaCalculada = 0;
+    let totalRecaudadoCalculado = 0; // Calcular total basado en ventas
+    const productoContador = {};
 
-			venta.productos.forEach((producto) => {
-				gananciaVenta += (producto.venta - producto.costo) * producto.cantidad;
-				totalRecaudadoCalculado += producto.venta * producto.cantidad;
-				productoContador[producto.nombre] = (productoContador[producto.nombre] || 0) + producto.cantidad;
-			});
+    ventas.forEach((venta) => {
+      let gananciaVenta = 0;
+      venta.productos.forEach((producto) => {
+        gananciaVenta += (producto.venta - producto.costo) * producto.cantidad;
+        productoContador[producto.nombre] = (productoContador[producto.nombre] || 0) + producto.cantidad;
+      });
 
-			totalGananciaCalculada += gananciaVenta;
-		});
+      // Sumar total recaudado por venta directamente
+      totalRecaudadoCalculado += venta.precioTotal;
+      totalGananciaCalculada += gananciaVenta;
+    });
 
-		setTotalGanancia(totalGananciaCalculada);
-		setTotalRecaudado(totalRecaudadoCalculado);
+    setTotalGanancia(totalGananciaCalculada);
+    setTotalRecaudado(totalRecaudadoCalculado);
 
-		// Verifica si hay productos vendidos antes de usar reduce
-		if (Object.keys(productoContador).length > 0) {
-			const productoMax = Object.keys(productoContador).reduce((a, b) =>
-				productoContador[a] > productoContador[b] ? a : b
-			);
-			setProductoMasVendido(productoMax);
-		} else {
-			setProductoMasVendido('No hay ventas.');
-		}
-	};
+    // Producto más vendido
+    if (Object.keys(productoContador).length > 0) {
+      const productoMax = Object.keys(productoContador).reduce((a, b) =>
+        productoContador[a] > productoContador[b] ? a : b
+      );
+      setProductoMasVendido(productoMax);
+    } else {
+      setProductoMasVendido('No hay ventas.');
+    }
+  };
 
-	// Función para eliminar una venta
-	const handleDeleteVenta = async (id) => {
-		Swal.fire({
-			title: '¿Estás seguro?',
-			text: 'No podrás revertir esto',
-			icon: 'warning',
-			showCancelButton: true,
-			confirmButtonColor: '#3085d6',
-			cancelButtonColor: '#d33',
-			confirmButtonText: 'Sí, eliminar',
-			cancelButtonText: 'Cancelar',
-		}).then(async (result) => {
-			if (result.isConfirmed) {
-				try {
-					await Axios.delete(`/venta/${id}`);
-					setVentas(ventas.filter((venta) => venta.uid !== id));
-					Swal.fire('¡Eliminada!', 'La venta ha sido eliminada.', 'success');
-				} catch (error) {
-					console.error('Error al eliminar la venta:', error);
-					Swal.fire('Error', 'No se pudo eliminar la venta.', 'error');
-				}
-			}
-		});
-	};
+  // Función para eliminar una venta
+  const handleDeleteVenta = async (id) => {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'No podrás revertir esto',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar',
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          await Axios.delete(`/venta/${id}`);
+          setVentas(ventas.filter((venta) => venta.uid !== id));
+          Swal.fire('¡Eliminada!', 'La venta ha sido eliminada.', 'success');
+        } catch (error) {
+          console.error('Error al eliminar la venta:', error);
+          Swal.fire('Error', 'No se pudo eliminar la venta.', 'error');
+        }
+      }
+    });
+  };
 
-	// Manejadores de paginación
-	const handlePreviousPage = () => {
-		if (currentPage > 1) {
-			setCurrentPage(currentPage - 1);
-		}
-	};
+  // Manejadores de paginación
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
-	const handleNextPage = () => {
-		if (currentPage < totalPages) {
-			setCurrentPage(currentPage + 1);
-		}
-	};
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
-	return (
-		<div className='container animate__animated animate__fadeIn my-5'>
-			<h2 className='text-center mb-4'>Reporte de Ventas</h2>
+  return (
+    <div className="container  my-5">
+      <h2 className="text-center mb-4">Reporte de Ventas</h2>
 
-			{/* Filtro por Fecha */}
-			<div className='d-flex justify-content-center mb-4'>
-				<DatePicker
-					selected={startDate}
-					onChange={(date) => setStartDate(date)}
-					dateFormat='dd/MM/yyyy'
-					className='form-control'
-				/>
-			</div>
+      {/* Filtro por Fecha */}
+      <div className="d-flex justify-content-center mb-4">
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          dateFormat="dd/MM/yyyy"
+          className="form-control"
+        />
+      </div>
 
-			{/* Total Recaudado, Ganancia Total y Producto Más Vendido */}
-			<div className='mb-4'>
-				<p>
-					<strong>Total Recaudado del Día:</strong> ${totalRecaudado.toFixed(2)} CUP
-				</p>
-				<p>
-					<strong>Ganancia Total del Día:</strong> ${totalGanancia.toFixed(2)} CUP
-				</p>
-				<p>
-					<strong>Producto Más Vendido:</strong> {productoMasVendido || 'No hay ventas.'}
-				</p>
-			</div>
+      {/* Total Recaudado, Ganancia Total y Producto Más Vendido */}
+      <div className="mb-4">
+        <p>
+          <strong>Total Recaudado del Día:</strong> ${totalRecaudado.toFixed(2)} CUP
+        </p>
+        <p>
+          <strong>Ganancia Total del Día:</strong> ${totalGanancia.toFixed(2)} CUP
+        </p>
+        <p>
+          <strong>Producto Más Vendido:</strong> {productoMasVendido || 'No hay ventas.'}
+        </p>
+      </div>
 
-			{/* Tabla de Ventas */}
-			<table className='table table-striped table-bordered'>
-				<thead className='thead-dark'>
-					<tr>
-						<th>#</th>
-						<th>Fecha</th>
-						<th>Productos</th>
-						<th>Total Recaudado</th>
-						<th>Ganancia por Venta</th>
-						<th>Gestor</th>
-						<th>Acciones</th>
-					</tr>
-				</thead>
-				<tbody>
-					{ventas.length === 0 ? (
-						<tr>
-							<td colSpan='7' className='text-center'>
-								No hay ventas para la fecha seleccionada.
-							</td>
-						</tr>
-					) : (
-						ventas.map((venta, index) => {
-							let gananciaVenta = 0;
+      {/* Contenedor con desplazamiento horizontal para la tabla */}
+      <div className="report-ventas__table-container">
+        <table className="table report-ventas__table table-striped table-bordered">
+          <thead className="thead-dark">
+            <tr>
+              <th>#</th>
+              <th>Fecha</th>
+              <th>Productos</th>
+              <th>Total Recaudado</th>
+              <th>Ganancia por Venta</th>
+              <th>Gestor</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ventas.length === 0 ? (
+              <tr>
+                <td colSpan="7" className="text-center">
+                  No hay ventas para la fecha seleccionada.
+                </td>
+              </tr>
+            ) : (
+              ventas.map((venta, index) => {
+                let gananciaVenta = 0;
 
-							venta.productos.forEach((producto) => {
-								gananciaVenta += (producto.venta - producto.costo) * producto.cantidad;
-							});
+                venta.productos.forEach((producto) => {
+                  gananciaVenta += (producto.venta - producto.costo) * producto.cantidad;
+                });
 
-							return (
-								<tr key={venta.uid}>
-									<td>{(currentPage - 1) * 8 + index + 1}</td>
-									<td>{new Date(venta.fecha).toLocaleDateString()}</td>
-									<td>
-										<ul>
-											{venta.productos.map((producto, idx) => (
-												<li key={idx}>
-													{producto.nombre} - {producto.cantidad} x $
-													{producto.venta.toFixed(2)}
-												</li>
-											))}
-										</ul>
-									</td>
-									<td>${venta.precioTotal.toFixed(2)} CUP</td>
-									<td>${gananciaVenta.toFixed(2)} CUP</td>
-									<td>{venta.gestor}</td>
-									<td>
-										<button className='btn btn-danger' onClick={() => handleDeleteVenta(venta.uid)}>
-											<FontAwesomeIcon icon={faTrashAlt} />
-											Eliminar
-										</button>
-									</td>
-								</tr>
-							);
-						})
-					)}
-				</tbody>
-			</table>
+                return (
+                  <tr key={venta.uid}>
+                    <td>{(currentPage - 1) * 8 + index + 1}</td>
+                    <td>{new Date(venta.fecha).toLocaleDateString()}</td>
+                    <td>
+                      <ul>
+                        {venta.productos.map((producto, idx) => (
+                          <li key={idx}>
+                            {producto.nombre} - {producto.totalProductos} x ${producto.venta.toFixed(2)}
+                          </li>
+                        ))}
+                      </ul>
+                    </td>
+                    <td>${venta.precioTotal.toFixed(2)} CUP</td>
+                    <td>${gananciaVenta.toFixed(2)} CUP</td>
+                    <td>{venta.gestor}</td>
+                    <td>
+                      <button className="btn btn-danger" onClick={() => handleDeleteVenta(venta.uid)}>
+                        <FontAwesomeIcon icon={faTrashAlt} />
+                        Eliminar
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })
+            )}
+          </tbody>
+        </table>
+      </div>
 
-			{/* Componente de Paginación */}
-			<Pagination
-				currentPage={currentPage}
-				totalPages={totalPages}
-				handlePreviousPage={handlePreviousPage}
-				handleNextPage={handleNextPage}
-				className='mt-4'
-			/>
-		</div>
-	);
+      {/* Componente de Paginación */}
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        handlePreviousPage={handlePreviousPage}
+        handleNextPage={handleNextPage}
+        className="mt-4"
+      />
+    </div>
+  );
 };
 
 export default ReporteVentas;
