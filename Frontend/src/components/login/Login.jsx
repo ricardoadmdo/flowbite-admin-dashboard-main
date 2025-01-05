bb9b996beff59d26fed2c8f2563f635ce84e6139
ricardoadmdo <descoubet0107@gmail.com>
2025-01-05 10:09:18 -0500

    a

--- Frontend/src/components/login/Login.jsx
+++ Frontend/src/components/login/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
 					toast: true,
 					position: "top-end",
 					title: "Éxito",
-					text: "Inicio de sesión exitos 🧑🏻",
+					text: "Inicio de sesión exitoso 🧑🏻",
 					showConfirmButton: false,
 					icon: "success",
 					timer: 3000,
